4ea18448f11ff77703eccb4a4f3e0b6e3f59ee7c
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-07-07 23:11:34 +0530

    database done

---
+++ src/data.js
@@ -0,0 +1,163 @@
+export const smartphoneData = {
+    "Best under 10000": [
+        {
+           name: "Redmi 9 Power",
+            storage variant: "4/64",
+            price: "₹10,499",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-9-power-0.jpg"
+        },
+        {
+            name: "Samsung Galaxy F12",
+            storage variant: "4/64",
+            price: "₹10,499",
+            img: "https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-f12-1.jpg"
+        }, 
+        {
+            name: "Micromax In 1",
+            storage variant: "4/64",
+            price: "9,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/micromax/micromax-in1-20.jpg"
+        }, 
+        {
+            name: "Motorola Moto G10 Power",
+            storage variant: "4/64",
+            price: "9,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/motorola/motorola-moto-g10-power-0.jpg"
+        },
+        {
+            name: "Realme Narzo 30A",
+            storage variant: "3/32",
+            price: "8,499",
+            img: "https://fdn2.gsmarena.com/vv/pics/realme/realme-narzo-30a-1.jpg"
+        }
+    ],
+    "Best under 15000": [
+        {
+            name: "Realme Narzo 30 5G",
+            storage variant: "6/128",
+            price: "15,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/realme/realme-narzo-30-5g-1.jpg"
+        },
+        {
+            name: "Redmi Note 10S",
+            storage variant: "6/64",
+            price: "14,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-note10s-1.jpg"
+        },
+        {
+            name: "Motorola Moto G40 Fusion",
+            storage variant: "4/64",
+            price: "14,499",
+            img: "https://fdn2.gsmarena.com/vv/pics/motorola/motorola-moto-g40-fusion-1.jpg"
+        },
+        {
+            name: "Poco M3 Pro 5G",
+            storage variant: "4/64",
+            price: "13,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-poco-m3-pro-5g-3.jpg"
+        },
+        {
+            name: "Realme 8",
+            storage variant: "4/128",
+            price: "13,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/realme/realme-8-1.jpg"
+        }
+    ],
+    "Best under 20000": [
+        {
+            name: "IQOO Z3 5G",
+            storage variant: "6/128",
+            price: "19,990",
+            img: "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-qoo-z3-0.jpg" 
+        },
+        {
+            name: "Redmi Note 10 Pro Max",
+            storage variant: "6/128",
+            price: "19,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-note10-pro-india-10.jpg" 
+        },
+        {
+            name: "Poco X3 Pro",
+            storage variant: "6/128",
+            price: "18,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-poco-x3-pro-1.jpg"
+        },
+        {
+            name: "Realme Narzo 30 Pro 5G",
+            storage variant: "8/128",
+            price: "18,499",
+            img: "https://fdn2.gsmarena.com/vv/pics/realme/realme-narzo-30-pro-3.jpg"
+        },
+        {
+            name: "Realme X7 5G",
+            storage variant: "6/128",
+            price: "17,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/realme/realme-x7-india-1.jpg"
+        },
+    ],
+    "Best under 30000": [
+        {
+            name: "IQOO 7 5G",
+            storage variant: "8/128",
+            price: "31,990",
+            img: "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-iqoo-7-india-3.jpg"
+        },
+        {
+            name: "Mi 11X 5G",
+            storage variant: "6/128",
+            price: "29,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-mi11x-1.jpg"
+        },
+        {
+            name: "Realme X7 Max 5G",
+            storage variant: "8/128",
+            price: "26,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/realme/realme-x7-max-5g-1.jpg"
+        },
+        {
+            name: "One Plus Nord CE",
+            storage variant: "8/128",
+            price: "24,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/oneplus/oneplus-nord-ce-5g-1.jpg"
+        },
+        {
+            name: "Mi 11 Lite",
+            storage variant: "6/128",
+            price: "21,999",
+            img: "https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-mi-11-lite-4g-1.jpg"
+        }
+    ],
+    "Best under 40000": [
+        {
+            name: "IQOO 7 Legend 5G",
+            storage variant: "6/128",
+            price: "21,999",
+            img:
+        },
+        {
+            name: "Mi 11X Pro",
+            storage variant: "6/128",
+            price: "21,999",
+            img:
+        },
+        {
+            name: "Mi 11 Lite",
+            storage variant: "6/128",
+            price: "21,999",
+            img:
+        },
+        {
+            name: "Mi 11 Lite",
+            storage variant: "6/128",
+            price: "21,999",
+            img:
+        },
+        {
+            name: "Mi 11 Lite",
+            storage variant: "6/128",
+            price: "21,999",
+            img:
+        }
+    ]
+
+}
