21768e03460580c9b46e2bb5c06745c240eb781b
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-08-31 19:28:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 import { smartphoneData } from "./data"
 
 function App() {
+
+  const [category, setCategory] = useState("Best under 20000")
+
+  // function getCategory(value){
+  //   setCategory(value);
+  // }
+
+  const data1 = (Object.keys(smartphoneData))
   return (
     <div className="App">
       <h1>SmartPhone Recommendation App</h1>
-      <h2>Best SmartPhones different Price range available in India</h2>
+      <h2>Best SmartPhones under different Price range available in India</h2>
+      <div className ="btn-container">
+      {
+        data1.map(value => <button onClick = {() => setCategory(value)} className ="btn">{value}</button>)
+      }
+      </div>
+        {
+          smartphoneData[category].map(value => (
+            <div className = "details">
+              <h3>{value.name}</h3>
+              <img className ="image" src={value.img} alt="" />
+              <h4>Storage Variant:- {value.storage_variant}</h4>
+              <h4>Price:- {value.price}</h4>
+            </div>
+          ))
+        }
     </div>
   );
 }
